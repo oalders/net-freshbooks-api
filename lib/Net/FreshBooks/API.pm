13fdf9df168dc1d72384617602f629144f29afc8
Olaf Alders <olaf@raybec.com>
2009-11-23 17:35:13 -0500

    bumped up version number for release

--- lib/Net/FreshBooks/API.pm
+++ lib/Net/FreshBooks/API.pm
@@ -4,7 +4,7 @@ use base 'Class::Accessor::Fast';
 use strict;
 use warnings;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use Carp qw( carp croak );
 use URI;
@@ -31,7 +31,7 @@ Net::FreshBooks::API - Easy OO access to the FreshBooks.com API
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =head1 SYNOPSIS
 
